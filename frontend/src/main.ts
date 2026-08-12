dbd1836dacc799b133795b2eeecc3197bb4c0f03
xiaqb <xiaqiubo123@163.com>
2026-08-12 17:55:51 +0800

    feat: add font import and global vuetify overrides

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/fonts.css'
 import './styles/tokens.css'
+import './styles/overrides.scss'
 
 createApp(App).use(router).mount('#app')
