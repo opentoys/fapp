aad65031c066348f9a12302960d2e8e293f17416
xiaqb <xiaqiubo123@163.com>
2026-08-12 15:19:44 +0800

    feat: backend scaffold with config loader

---
+++ backend/go.mod
@@ -0,0 +1,27 @@
+module disapp
+
+go 1.25.0
+
+require (
+	github.com/clbanning/mxj v1.8.4 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/libtnb/sqlite v1.2.2 // indirect
+	github.com/mattn/go-isatty v0.0.24 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/mozillazg/go-httpheader v0.2.1 // indirect
+	github.com/ncruces/go-strftime v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/tencentyun/cos-go-sdk-v5 v0.7.75 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.39.0 // indirect
+	gorm.io/gorm v1.31.2 // indirect
+	modernc.org/libc v1.74.4 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
+	modernc.org/sqlite v1.56.0 // indirect
+)
---
+++ backend/internal/config/config.go
@@ -0,0 +1,78 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Server   ServerConfig   `json:"server"`
+	Database DatabaseConfig `json:"database"`
+	Storage  StorageConfig  `json:"storage"`
+	JWT      JWTConfig      `json:"jwt"`
+}
+
+type ServerConfig struct {
+	Addr string `json:"addr"`
+}
+
+type DatabaseConfig struct {
+	DSN string `json:"dsn"`
+}
+
+type StorageConfig struct {
+	Backend string      `json:"backend"`
+	Local   LocalConfig `json:"local"`
+	COS     COSConfig   `json:"cos"`
+}
+
+type LocalConfig struct {
+	Dir string `json:"dir"`
+}
+
+type COSConfig struct {
+	SecretID  string `json:"secret_id"`
+	SecretKey string `json:"secret_key"`
+	Bucket    string `json:"bucket"` // 如 app-dist-1250000000
+	Region    string `json:"region"` // 如 ap-guangzhou
+	BaseURL   string `json:"base_url"`
+}
+
+type JWTConfig struct {
+	Secret string `json:"secret"`
+	Expire string `json:"expire"` // Go duration 字符串，如 "24h"
+}
+
+func Default() Config {
+	return Config{
+		Server:   ServerConfig{Addr: ":8080"},
+		Database: DatabaseConfig{DSN: "./data/app.db"},
+		Storage:  StorageConfig{Backend: "local", Local: LocalConfig{Dir: "./data/files"}},
+		JWT:      JWTConfig{Secret: "change-me", Expire: "24h"},
+	}
+}
+
+func Load(path string) (Config, error) {
+	data, err := os.ReadFile(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return Default(), nil
+	}
+	if err != nil {
+		return Config{}, err
+	}
+	c := Default()
+	if err := json.Unmarshal(data, &c); err != nil {
+		return Config{}, err
+	}
+	return c, nil
+}
+
+func (c Config) JWTExpire() time.Duration {
+	d, err := time.ParseDuration(c.JWT.Expire)
+	if err != nil {
+		return 24 * time.Hour
+	}
+	return d
+}
