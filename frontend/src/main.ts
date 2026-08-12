e7b2d482425dddd7fabeb937690cf9d7c86d626a
xiaqb <xiaqiubo123@163.com>
2026-08-12 17:56:13 +0800

    feat: register vuetify plugin with dark and light themes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { vuetify } from './plugins/vuetify'
+import 'vuetify/styles'
 import './styles/fonts.css'
 import './styles/tokens.css'
 import './styles/overrides.scss'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
